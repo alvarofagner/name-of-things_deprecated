7941bb0a447611c3b73d680e9729e7b4c56dc7fb
Alvaro Silva <alvaro.fagner@gmail.com>
2016-11-19 19:42:08 +0100

    Creating initial project

---
+++ Assets/CameraSetup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+
+public class CameraSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Starting script");
+		VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		VuforiaBehaviour.Instance.RegisterOnPauseCallback(OnPaused);
+	}
+
+	private void OnVuforiaStarted()
+	{
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_MACRO);
+	}
+
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_MACRO);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
